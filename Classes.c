4ac23b1384113375af707df0e642c44f16d4aac8
ZM <mukaddamzayd@gmail.com>
2024-05-12 00:17:52 -0400

    Updated Classes.c to travese a 2d array.

--- Classes.c
+++ Classes.c
@@ -10,4 +10,14 @@ int main(){
 	printf("Hi there");
 	}
 
+	bool graph[10][10];
+
+	for (int i = 0; i < sizeof(graph)/sizeof(graph[i]); i++){
+		for (int j = 0; j < sizeof(graph[i])/sizeof(graph[i][j]); j++){
+			printf(graph[i][j] + " ");
+		}
+		printf("\n");
+	}
+	return 0;
+
 }
